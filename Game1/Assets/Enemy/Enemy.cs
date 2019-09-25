3dabe1b3d4a825b710fec84eaa91b078c6866347
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2019-09-25 21:22:30 +0300

    логика для врага

---
+++ Game1/Assets/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Text_Dead text;
+    public GameObject text_end;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) //если объект столкнулся с игроком
+        {
+                text_end.SetActive(true);  //то активируем объект, который будет выводить текст на экран
+                text.game_end = true; //и посылаем команду другому скрипту, что конец игры это правда
+        }
+    }
+}
---
+++ Game1/Assets/Enemy/Text_Dead.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Text_Dead : MonoBehaviour
+{
+    public bool game_end = false;
+    public string text_end;
+    public Text end;
+    public float time_end = 5;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        end.text = text_end; //какой именно текст выводим на экран
+
+        if (game_end == true) //если конец игры правда
+        {
+            time_end -= Time.deltaTime; //активация отсчета времени до перезапуска сцены
+            if (time_end <= 0) //если счетчик равен или меньше нуля
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); //перезапускаем текущую сцену
+            }
+        }
+    }
+}
