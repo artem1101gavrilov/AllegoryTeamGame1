293aeefe64055548931aed0ac83330b6ccc44ceb
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2019-09-26 00:30:22 +0300

    сделал несколько видов врагов (недвижимый, с анимацией, с передвижением вперед и назад и тот, который следует за игроком)

---
+++ Game1/Assets/Enemy/Enemy2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    //в общем встречайте мой очередной говнокод, но зато рабочий :)
+
+    public int distance; //чем больше значение - тем больше времени объект будет двигаться в одну сторону
+    float time; //отсчет времени
+    int dir = 0; //изменение направления объекта и отсчета времени (в отрицательную или положительную сторону)
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dir==0) //присваиваем времени число дистанции и начинаем отсчитывать
+        {
+            time = distance;
+            dir = 1;
+        }
+        if (time >= distance) //больше этого числа нельзя, поэтому меняем направление объекта и отсчитываем время назад
+        {
+            dir = 1;
+        }
+        if (time <= 0) //меньше этого числа нельзя, поэтому меняем направление объекта и отсчитываем время вперед
+        {
+            dir = 2;
+        }
+        if (dir == 1) //направляем объект влево и отсчитываем время до нуля
+        {
+            time -= Time.deltaTime;
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        if (dir == 2) //направляем объект вправо и отсчитываем время до определенного числа
+        {
+            time += Time.deltaTime;
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+    }
+}
---
+++ Game1/Assets/Enemy/Enemy_direction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_direction : MonoBehaviour
+{
+    //движение объекта к игроку
+
+    GameObject player;
+
+    public float speed = 1;
+
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void Update()
+    {
+        float direction = player.transform.position.x - transform.position.x;
+
+        if (Mathf.Abs(direction) < 20)
+        {
+            Vector2 pos = transform.position;
+            pos.x += Mathf.Sign(direction) * speed * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
