fc29ef3d7947db96723eb5d62b0d4277ae0df930
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2019-09-27 15:48:36 +0300

    проделана работа: -сделал меню -загрузил ассеты и перенес их в одну папку -исправил баг

---
+++ Game1/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject AutorsMenu;
+    public GameObject ControlMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickGame()
+    {
+        SceneManager.LoadScene("Enemy_test");
+    }
+    public void ClickAutors()
+    {
+        MainMenu.active = false;
+        AutorsMenu.active = true;
+    }
+    public void ClickControl()
+    {
+        MainMenu.active = false;
+        ControlMenu.active = true;
+    }
+    public void ClickExit()
+    {
+        
+    }
+    public void ClickBack()
+    {
+        MainMenu.active = true;
+        AutorsMenu.active = false;
+        ControlMenu.active = false;
+    }
+}
